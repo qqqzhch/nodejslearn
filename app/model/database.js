56d563537004e0e103bea35dc17fa70d0ecf4dfe
webfans <qqqzhch@163.com>
2015-05-29 21:23:24 +0800

    修改数据库连接方式

--- app/model/database.js
+++ app/model/database.js
@@ -1,13 +1,17 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV,
 	config = require('../../config/config')[env]
-module.exports = new Sequelize(config.postsql.dbname,
+module.exports = new Sequelize(
+	config.postsql.dbname,
 	config.postsql.username,
-	config.postsql.password,
-	config.postsql.dbType,
-	pool: {
-		max: 50,
-		min: 0,
-		idle: 10000
+	config.postsql.password, {
+		host: config.postsql.dbType.host,
+		dialect: config.postsql.dbType.dialect,
+		pool: {
+			max: 50,
+			min: 0,
+			idle: 10000
+		}
 	}
+
 );
