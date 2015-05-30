c906dc68ec65da919c82c4ba2a716fd5125b0839
webfans <qqqzhch@163.com>
2015-05-30 09:09:15 +0800

    tongji daima

--- app/job/cron.js
+++ app/job/cron.js
@@ -2,31 +2,31 @@ var CronJob = require('cron').CronJob;
 console.log('kaishi');
 
 console.log(process.env.NODE_ENV);
-var github=require('./github');
+var github = require('./github');
 // github.run()
-new CronJob('*4 * * * *', function(){
+new CronJob('*4 * * * *', function() {
     console.log('4min  search github');
     // github.run()
 }, null, true, "America/Los_Angeles");
 
 
-new CronJob('*3 * * * *', function(){
+new CronJob('*3 * * * *', function() {
     console.log('2min  search youtube');
     require('./youtube').run()
 }, null, true, "America/Los_Angeles");
 
-new CronJob('*3 * * * *', function(){
+new CronJob('*3 * * * *', function() {
     console.log('2min  search stackSearch');
     require('./stackSearch').run()
 }, null, true, "America/Los_Angeles");
 
 
-new CronJob('*3 * * * *', function(){
+new CronJob('*3 * * * *', function() {
     console.log('2min  search youtubeVideo');
     require('./youtubeVideo').run()
 }, null, true, "America/Los_Angeles");
 
-new CronJob('*2 * * * *', function(){
+new CronJob('*2 * * * *', function() {
     console.log('2min  search stackAnswer');
     require('./stackAnswer').run()
 }, null, true, "America/Los_Angeles");
