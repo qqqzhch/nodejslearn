a43ea35fe619aed4d7bf4d1f449fbf9b49495619
webfans <qqqzhch@163.com>
2015-05-30 10:22:43 +0800

    404 to 302

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,11 @@
 var home = require('../app/controllers/home');
 var OpenSource = require('../app/controllers/OpenSource');
 var question = require('../app/controllers/question');
-var video= require('../app/controllers/video');
- 
+var video = require('../app/controllers/video');
 
 
-module.exports = function(app,config) {
+
+module.exports = function(app, config) {
     console.log('luyou');
     //app.get('/',home.index );
 
@@ -13,7 +13,7 @@ module.exports = function(app,config) {
     app.get('/error', home.error);
     app.get('/info', home.info);
     app.get('/rlist', home.rlist);
-    
+
 
     app.get('/opensource/:pager(\\d+)?', OpenSource.index);
     app.get('/opensource/:ower/:repo', OpenSource.repo);
@@ -21,8 +21,7 @@ module.exports = function(app,config) {
     app.get('/opensource/:ower/:repo/video/:pager(\\d+)?', video.list);
     app.get('/opensource/:ower/:repo/q/:id_site/:id_question/:title?', question.info);
     app.get('/opensource/:ower/:repo/v/:id_site/:id_video/:title?', video.info);
-    
-    
+
 
 
     app.get('/github/repos', home.repos);
@@ -30,9 +29,15 @@ module.exports = function(app,config) {
     app.get('/github/getConent', home.getConent);
     app.get('/stack/getSearch', home.getSearch);
     app.get('/stack/getQuestionsInfo', home.getQuestionsInfo);
-    app.get('/stack/getAnswersnfo', home.getAnswersnfo );
-    app.get('/youtube/search', home.search  );
-    app.get('/youtube/videos', home.videos  );
-    
-    
+    app.get('/stack/getAnswersnfo', home.getAnswersnfo);
+    app.get('/youtube/search', home.search);
+    app.get('/youtube/videos', home.videos);
+    app.get('*', function(req, res) {
+        res.writeHead(302, {
+            'Location': '/'
+        });
+        res.end();
+    });
+
+
 }
