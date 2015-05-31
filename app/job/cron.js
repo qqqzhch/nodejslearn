f3781ecfda1e52434d5961da70bf5193d1331178
webfans <qqqzhch@163.com>
2015-05-31 13:10:27 +0800

    job all

--- app/job/cron.js
+++ app/job/cron.js
@@ -10,30 +10,30 @@ new CronJob('*4 * * * *', function() {
 }, null, true, "America/Los_Angeles");
 
 
-// new CronJob('*1 * * * *', function() {
-//     console.log('2min  search youtube');
-//     require('./youtube').run()
-// }, null, true, "America/Los_Angeles");
+new CronJob('*1 * * * *', function() {
+    console.log('2min  search youtube');
+    require('./youtube').run()
+}, null, true, "America/Los_Angeles");
 
-// new CronJob('*2 * * * *', function() {
-//     console.log('2min  search stackSearch');
-//     require('./stackSearch').run()
-// }, null, true, "America/Los_Angeles");
+new CronJob('*2 * * * *', function() {
+    console.log('2min  search stackSearch');
+    require('./stackSearch').run()
+}, null, true, "America/Los_Angeles");
 
 
-// new CronJob('*2 * * * *', function() {
-//     console.log('2min  search youtubeVideo');
-//     require('./youtubeVideo').run()
-// }, null, true, "America/Los_Angeles");
+new CronJob('*2 * * * *', function() {
+    console.log('2min  search youtubeVideo');
+    require('./youtubeVideo').run()
+}, null, true, "America/Los_Angeles");
 
-// new CronJob('*2 * * * *', function() {
-//     console.log('2min  search stackAnswer');
-//     require('./stackAnswer').run()
-// }, null, true, "America/Los_Angeles");
+new CronJob('*2 * * * *', function() {
+    console.log('2min  search stackAnswer');
+    require('./stackAnswer').run()
+}, null, true, "America/Los_Angeles");
 
-// new CronJob('*2 * * * *', function() {
-//     console.log('2min  search gitReadme');
-//     require('./gitReadme').run()
-// }, null, true, "America/Los_Angeles");
+new CronJob('*2 * * * *', function() {
+    console.log('2min  search gitReadme');
+    require('./gitReadme').run()
+}, null, true, "America/Los_Angeles");
 
 console.log('kaishiend');
