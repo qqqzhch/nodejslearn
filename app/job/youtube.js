7260afafacba856a971c56b9a9b3b723df8a0c93
webfans <qqqzhch@163.com>
2015-06-02 07:42:45 +0800

    job

--- app/job/youtube.js
+++ app/job/youtube.js
@@ -16,7 +16,7 @@ function getresplist(callbaack) {
     gitRepoSql
         .findAll({
             offset: 0,
-            limit: 2,
+            limit: 10,
             order: '"createdAt" ASC',
             where :["viedo_num is NULL"]
         })
