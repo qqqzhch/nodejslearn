eb67a5a372322a583c0f7574dde379b272e22069
webfans <qqqzhch@163.com>
2016-05-11 13:20:09 +0800

    new num

--- app/job/hacknewsjob.js
+++ app/job/hacknewsjob.js
@@ -19,7 +19,7 @@ function getresplist(callbaack) {
     gitRepoSql
         .findAll({
             offset: 0,
-            limit: 2,
+            limit: 4,
             order: '"stargazers_count" desc',
             where: ["story_num is NULL"]
         })
@@ -45,7 +45,7 @@ function getBookSearch(list, callbaack) {
             nameForSearch += "    " + aitem.owner.login
         }
         console.log('采集news ' + nameForSearch);
-        hn.story().search(nameForSearch).hitsPerPage(500).before('past_week', function(error, data) {
+        hn.story().search(nameForSearch).hitsPerPage(150).before('past_week', function(error, data) {
             if (error) {
                 //cuowu
                 console.log(error);
